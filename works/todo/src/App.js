9543e7907c097456d4b5316df29d6e914f181a02
kamaeguchi <lhz.ca2@gmail.com>
2020-05-26 01:32:27 +0900

    package.json 更新

--- works/todo/src/App.js
+++ works/todo/src/App.js
@@ -1,69 +1,101 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import TextField from '@material-ui/core/TextField'
+import './App.css';
 
-function List(props){
-  return (
-    <ul>
-      {this.state.todo.map((todo, i) => {
-      return (
-        <li>
-          <input type="button" value="×" onClick={this.deleteTodo(i)} />
-          {todo.title}<br />
-          {todo.contents}
-        </li>
-      )
-    })}
-    </ul>    
-  )
-}
- 
-class App extends Component {
+
+class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      title: '',
+      memo: '',
       todo: [
-        {title: 'javascript', contents: 'ES2016覚える'},
-        {title: 'VueJS', contents: 'データバインド覚える'},
-        {title: 'React', contents: 'JSX覚える'},
-        {title: 'Angular', contents: '？？'}
+        {title: '買出し', memo: '牛乳を買う'},
+        {title: '家賃振込み', memo: '○○銀行'}
       ]
     };
+    this.inputChange = this.inputChange.bind(this);
     this.addTodo = this.addTodo.bind(this);
   }
+  // 値を保持
+  inputChange(e) {
+    let name = e.target.name; // フォームのname属性を取得 
+    this.setState({
+      [name]: e.target.value
+    })
+  }
+
+  // 新規追加
   addTodo() {
     // 追加
     this.state.todo.push({
-      title: this.refs.newText.value,
-      contents: this.refs.newTextArea.value
+      title: this.state.title,
+      memo: this.state.memo
     });
     // 保存
     this.setState({
-      todo: this.state.todo
+      todo : this.state.todo,
+      title : '', // 初期化
+      memo : ''   // 初期化
     });
-    // 初期化
-    this.refs.newText.value='';
-    this.refs.newTextArea.value='';
   }
-  
+ 
   // 削除機能
-  deleteTodo(i){
+  deleteTodo(i) {
     // 削除
-    this.state.todo.splice(i, 1)
+    this.state.todo.splice(i, 1);
     // 保存
     this.setState({
-      todo: this.state.todo
+      todo : this.state.todo
     });
   }
+ 
   render() {
     return (
       <div>
-        <h1>TODOアプリ</h1>
-        <list todo={this.state.todo} />
-        <input type="text" ref="newText" />
-        <input type="textarea" ref="newTextArea" />
-        <input type="button" value="追加" onClick={this.addTodo} />
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography color="inherit">
+            TODOアプリ
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <TextField
+          name="title"
+          placeholder="タイトル"
+          onChange={this.inputChange}
+          value={this.state.title}
+        />
+        <TextField
+          name="memo"
+          placeholder="メモ"
+          onChange={this.inputChange}
+          value={this.state.memo}
+        />
+        <Button color="primary" size="large" variant="outlined" aria-label="add" onClick={this.addTodo}>
+          <AddIcon />
+        </Button>
+
+        <ul>
+          {this.state.todo.map( (todo, i) => {
+            return <li key={i}>
+                    {todo.title} - {todo.memo} 
+                    <Button aria-label="delete" onClick={() => this.deleteTodo(i)}>
+                      <DeleteIcon />
+                    </Button>
+                  </li>
+          })}
+        </ul>
+        {this.state.title} {this.state.memo}
       </div>
     );
   }
 }
- 
 export default App;
--- works/todo/src/index.js
+++ works/todo/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import 'ress';
 
 ReactDOM.render(
   <React.StrictMode>
